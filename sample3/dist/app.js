3f6eb5643d9ed1ec2994e42815c95261009b6e67
ganesh <webdeveloper.ganesh@gmail.com>
2020-11-29 12:43:29 +0530

    sample3 add for again understand

---
+++ sample3/dist/app.js
@@ -0,0 +1,9 @@
+"use strict";
+//Understand Function 
+// const greet=(name:string,age?:number):void=>{
+//     console.log(name);
+// }
+// const greet=(name:string,age?:number):string=>{
+//     return name;
+// }
+// console.log(greet("10"));
